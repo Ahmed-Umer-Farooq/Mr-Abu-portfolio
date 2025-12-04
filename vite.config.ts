052bf68b89210165b2a9eb94951c7dff63760c9a
Ahmed-Umer-Farooq <btumer83@gmail.com>
2025-12-04 22:51:54 +0500

    fix: correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Mr-Abu-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
